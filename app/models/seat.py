7acbf61623b6157ff83c61e1ae1abea03c099998
Taras Maleryk <taras.maleryk.dev@gmail.com>
2026-06-27 12:44:38 +0000

    feat: add seat model

---
+++ app/models/seat.py
@@ -0,0 +1,12 @@
+from app.db.session import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+class Seat(Base):
+    __tablename__ = "seats"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("events.id"), nullable=False)
+    row: Mapped[int] = mapped_column(nullable=False)
+    number: Mapped[int] = mapped_column(nullable=False)
+    price: Mapped[int] = mapped_column(nullable=False)
