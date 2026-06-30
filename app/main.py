85fbac297cd79c5c87eef2b092d4e537a9204c9c
Taras Maleryk <taras.maleryk.dev@gmail.com>
2026-06-30 15:48:24 +0000

    fix: register auth router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from app.routers.auth import router as auth_router
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+api_router = APIRouter(prefix="/api")
+api_router.include_router(auth_router)
+app.include_router(api_router)
